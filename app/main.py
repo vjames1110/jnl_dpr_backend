0a74edf37e5e9685e266a596c27f4155f3bb8cf5
vjames1110 <vikashjames8@gmail.com>
2026-04-23 18:18:37 +0530

    Fixed CORS issue

--- app/main.py
+++ app/main.py
@@ -19,8 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://jnl-dpr-backend.onrender.com",
-        "https://jnldpr.netlify.app/"
+        "https://jnldpr.netlify.app"
     ],  # allow frontend
     allow_credentials=True,
     allow_methods=["*"],
