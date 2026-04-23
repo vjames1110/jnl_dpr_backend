f8c3ebfa623521511d561bbdc48e66a3b469bcb6
vjames1110 <vikashjames8@gmail.com>
2026-04-23 18:08:16 +0530

    Minor Fix of URL

--- app/main.py
+++ app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://jnl-dpr-backend.onrender.com/"
+        "https://jnl-dpr-backend.onrender.com"
     ],  # allow frontend
     allow_credentials=True,
     allow_methods=["*"],
